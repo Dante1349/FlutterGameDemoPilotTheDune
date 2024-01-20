183badd1dfea78478366739239bef30c688809c1
Marc Rominger <Marcrominger@gmx.de>
2024-01-20 21:32:26 +0100

    some updates

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flame_tiled_utils/flame_tiled_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
@@ -42,6 +43,8 @@ class TiledGame extends FlameGame
     Flame.device.fullScreen();
     Flame.device.setLandscape();
 
+    //super.debugMode=true;
+
     camera.viewfinder
       ..zoom = _startZoom
       ..anchor = Anchor.topLeft;
@@ -56,7 +59,7 @@ class TiledGame extends FlameGame
     final image = await images.load('ui.png');
     final sheet = SpriteSheet.fromColumnsAndRows(
       image: image,
-      columns: 4,
+      columns: 7,
       rows: 2,
     );
 
@@ -69,24 +72,26 @@ class TiledGame extends FlameGame
       size: Vector2(32, 32),
     );
     final xButton = SpriteButtonComponent(
-        button: sheet.getSpriteById(2),
-        buttonDown: sheet.getSpriteById(6),
-        position: Vector2(500, 250),
-        size: Vector2(32, 32));
+        button: sheet.getSpriteById(5),
+        buttonDown: sheet.getSpriteById(12),
+        position: Vector2(camera.viewport.size.x-200, camera.viewport.size.y-200),
+        size: Vector2(32, 32)
+    );
     final yButton = SpriteButtonComponent(
-        button: sheet.getSpriteById(3),
-        buttonDown: sheet.getSpriteById(7),
-        position: Vector2(600, 250),
-        size: Vector2(32, 32));
+        button: sheet.getSpriteById(6),
+        buttonDown: sheet.getSpriteById(13),
+        position: Vector2(camera.viewport.size.x-350, camera.viewport.size.y-150),
+        size: Vector2(32, 32)
+    );
 
     knob.width = knob.width * 5;
     knob.height = knob.height * 5;
     background.width = background.width * 5;
     background.height = background.height * 5;
-    xButton.width = xButton.width * 5;
-    xButton.height = xButton.height * 5;
-    yButton.width = yButton.width * 5;
-    yButton.height = yButton.height * 5;
+    xButton.width = xButton.width * 3;
+    xButton.height = xButton.height * 3;
+    yButton.width = yButton.width * 3;
+    yButton.height = yButton.height * 3;
 
     _joystick = JoystickComponent(
       knob: knob,
@@ -141,11 +146,17 @@ class TiledGame extends FlameGame
   }
 
   void spawnObjects(RenderableTiledMap tileMap) {
-    final worldObjects = tileMap.getLayer<ObjectGroup>("Object Layer 1");
+    // TileProcessor.processTileType(tileMap: tileMap, processorByType: <String, TileProcessorFunc>{
+    //     '': ((tile, position, size) {
+    //       add(WorldObject(tile.getCollisionRect()!.position, tile.getCollisionRect()!.size));
+    //       return Future(() => null);
+    //     })
+    // }, layersToLoad: [""]);
 
-    for (final worldObject in worldObjects!.objects) {
-      add(WorldObject(Vector2(worldObject.x, worldObject.y),
-          Vector2(worldObject.width, worldObject.height)));
+    final objectGroup =  tileMap.getLayer<ObjectGroup>("objects");
+    for(final tile in objectGroup!.objects) {
+      add(WorldObject(Vector2(tile.x,tile.y), Vector2(tile.width, tile.height)));
     }
+  
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -9,6 +9,7 @@ class JoystickPlayer extends SpriteAnimationComponent
   double maxSpeed = 150.0;
   late final Vector2 _lastSize = size.clone();
   late final Transform2D _lastTransform = transform.clone();
+  late final Vector2 _lastPosition = position.clone();
   late final SpriteSheet _playerSpriteSheet;
 
   late SpriteAnimation playerAnimationIdle;
@@ -25,7 +26,7 @@ class JoystickPlayer extends SpriteAnimationComponent
   @override
   Future<void> onLoad() async {
 
-    final image = await game.images.load('player_sprite.png');
+    final image = await game.images.load('pilot-Sheet.png');
     _playerSpriteSheet = SpriteSheet.fromColumnsAndRows(
       image: image,
       columns: 16,
@@ -47,14 +48,14 @@ class JoystickPlayer extends SpriteAnimationComponent
     playerAnimationLeft = _playerSpriteSheet.createAnimation(
       stepTime: 0.1,
       row: 0,
-      from: 5,
-      to: 8,
+      from: 9,
+      to: 12,
     );
     playerAnimationRight = _playerSpriteSheet.createAnimation(
       stepTime: 0.1,
       row: 0,
-      from: 9,
-      to: 12,
+      from: 5,
+      to: 8,
     );
     playerAnimationDown = _playerSpriteSheet.createAnimation(
       stepTime: 0.1,
@@ -65,7 +66,7 @@ class JoystickPlayer extends SpriteAnimationComponent
 
     animation = playerAnimationIdle;
 
-    add(RectangleHitbox(size: Vector2(16,16)));
+    add(RectangleHitbox(size: Vector2(16,16), position: Vector2(16,16), anchor: Anchor.center));
     position = Vector2(game.camera.visibleWorldRect.width/2, game.camera.visibleWorldRect.height/2+400);
   }
 
@@ -74,14 +75,23 @@ class JoystickPlayer extends SpriteAnimationComponent
     if (!joystick.delta.isZero() && activeCollisions.isEmpty) {
       _lastSize.setFrom(size);
       _lastTransform.setFrom(transform);
+      _lastPosition.setFrom(position);
       position.add(joystick.relativeDelta * maxSpeed * dt);
 
       if(joystick.isDragged && joystick.direction == JoystickDirection.up) {
         animation = playerAnimationUp;
+      } else if (joystick.isDragged && joystick.direction == JoystickDirection.upLeft) {
+        animation = playerAnimationLeft;
+      } else if (joystick.isDragged && joystick.direction == JoystickDirection.upRight) {
+        animation = playerAnimationRight;
       } else if (joystick.isDragged && joystick.direction == JoystickDirection.right) {
         animation = playerAnimationRight;
       } else if (joystick.isDragged && joystick.direction == JoystickDirection.down) {
         animation = playerAnimationDown;
+      } else if (joystick.isDragged && joystick.direction == JoystickDirection.downLeft) {
+        animation = playerAnimationLeft;
+      } else if (joystick.isDragged && joystick.direction == JoystickDirection.downRight) {
+        animation = playerAnimationRight;
       } else if (joystick.isDragged && joystick.direction == JoystickDirection.left) {
         animation = playerAnimationLeft;
       } else {
@@ -96,6 +106,6 @@ class JoystickPlayer extends SpriteAnimationComponent
     PositionComponent other,
   ) {
     super.onCollisionStart(intersectionPoints, other);
-    transform.setFrom(_lastTransform);
+    position.setFrom(_lastPosition);
   }
 }
